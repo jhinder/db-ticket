4c5e7b3e71c44fea6049e7ba1b035428d9ab7b44
jhinder <jan@dfragment.net>
2015-01-18 00:20:02 +0100

    Corrected comment in header

--- db-ticket.h
+++ db-ticket.h
@@ -1,6 +1,11 @@
-/* dbticket/libmain.c
+/* dbticket/db-ticket.h
  * (c) jan (dfragment.net) 2015
  * Licensed under MIT licence.
+ */
+
+/* This is the header file for the db-ticket library.
+ * For information on usage, read the documentation that came with the library,
+ * or see the online documentation at http://github.com/jhinder/db-ticket.
  */
 
 #ifndef DB_TICKET_H
