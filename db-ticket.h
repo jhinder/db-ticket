9f9a76ded5aedc2e156330f9478cbb74fabb25da
jhinder <jan@dfragment.net>
2015-01-18 00:06:03 +0100

    Added official API

---
+++ db-ticket.h
@@ -0,0 +1,13 @@
+/* dbticket/libmain.c
+ * (c) jan (dfragment.net) 2015
+ * Licensed under MIT licence.
+ */
+
+#ifndef DB_TICKET_H
+#define DB_TICKET_H
+
+#define kMaximumScore 6.5f
+
+float checkPDFFile(const char *filename);
+
+#endif
---
+++ libmain.c
@@ -0,0 +1,43 @@
+/* dbticket/libmain.c
+ * (c) jan (dfragment.net) 2015
+ * Licensed under MIT licence.
+ */
+
+#include <stdio.h>
+#include "modules.h"
+#include "db-ticket.h"
+
+float checkPDFFile(const char *filename)
+{
+	FILE *fptr = fopen(filename, "rb");
+	if (fptr == NULL)
+		return -1;
+
+	float scorepoints = 0.0f;
+
+	// Check 1: file size.
+	if (hasAcceptableSize(fptr))
+		scorepoints += 0.5f;
+	else
+		return -2;
+
+	// Check 2: the /Producer string.
+	if (producerStringPresent(fptr)) {
+		scorepoints += 1.0f;
+	} else {
+		// might be a ticket with an HTML tail
+		if (trailerContainsHTML(fptr))
+			scorepoints += 0.25f;
+	}
+
+	if (hasCorrectPDFVersion(fptr))
+		scorepoints += 1.0f;
+
+	if (hasCorrectSignature(fptr))
+		scorepoints += 2.0f;
+
+	if (hasImageHexcode(fptr))
+		scorepoints += 2.0f;
+
+	return scorepoints;
+}
