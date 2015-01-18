0a7220718461141d1ba4558909254d9cc2e2260b
jhinder <jan@dfragment.net>
2015-01-18 22:27:07 +0100

    Fix for three bugs in three lines.

--- netfx-cs/db-pdftest/Program.cs
+++ netfx-cs/db-pdftest/Program.cs
@@ -11,16 +11,16 @@ namespace db_pdftest
     {
         static void Main(string[] args)
         {
-            if (args.Length < 2)
+            if (args.Length < 1)
             {
                 Console.WriteLine("Invalid argument count. You must pass the path to a file.\nPress a key to exit.");
                 Console.ReadKey();
                 return;
             }
                
-            TicketCheck tc_1 = new TicketCheck(args[1]);
+            TicketCheck tc_1 = new TicketCheck(args[0]);
             Console.Write("Result: ");
-            Console.Write(String.Format("{0} of {1} points\n"), tc_1.Result, TicketCheck.MaximumScore);
+            Console.Write(String.Format("{0} of {1} points\n", tc_1.Result, TicketCheck.MaximumScore));
 
             Console.WriteLine("Press a key to exit.");
             Console.ReadKey();
