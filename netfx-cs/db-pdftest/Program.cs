eedaa36060e5bebc1b4911191a7c981971687894
jhinder <jan@dfragment.net>
2015-01-18 22:10:30 +0100

    Added test tool for C# library

---
+++ netfx-cs/db-pdftest/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using dbticket;
+
+namespace db_pdftest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length < 2)
+            {
+                Console.WriteLine("Invalid argument count. You must pass the path to a file.");
+                return;
+            }
+               
+            TicketCheck tc_1 = new TicketCheck(args[1]);
+        }
+    }
+}
