8612f9a3c606156dda001f6039c491a3e809be93
jhinder <jan@dfragment.net>
2015-01-18 16:29:01 +0100

    Constructors for TicketCheck

--- netfx-cs/db-ticket/TicketCheck.cs
+++ netfx-cs/db-ticket/TicketCheck.cs
@@ -3,10 +3,39 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
-namespace db_ticket
+namespace dbticket
 {
     public class TicketCheck
     {
+
+        /// <summary>
+        /// Creates a new TicketCheck.
+        /// </summary>
+        /// <param name="inputStream">A FileStream representing the input file.</param>
+        public TicketCheck(FileStream inputStream)
+        {
+
+        }
+
+        /// <summary>
+        /// Creates a new TicketCheck.
+        /// </summary>
+        /// <param name="inputFile">A FileInfo representing the input file.</param>
+        public TicketCheck(FileInfo inputFile)
+            : this(inputFile.OpenRead())
+        {
+        }
+
+        /// <summary>
+        /// Creates a new TicketCheck.
+        /// </summary>
+        /// <param name="inputFilePath">The path to the input file.</param>
+        public TicketCheck(string inputFilePath)
+            : this(File.Open(inputFilePath, FileMode.Open))
+        {
+        }
+
     }
 }
