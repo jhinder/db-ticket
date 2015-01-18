de4bdbc946bcb72f5e0be9336297ae62e118bde7
jhinder <jan@dfragment.net>
2015-01-18 19:17:51 +0100

    Added exceptions & constants

---
+++ netfx-cs/db-ticket/Constants.cs
@@ -0,0 +1,14 @@
+﻿namespace dbticket
+{
+    /// <summary>
+    /// Defines some constants used in the algorithms
+    /// </summary>
+    internal static class Constants
+    {
+        public const string Producer = "";
+        public const string PDFVersion = "%PDF-1.4";
+        public const string PDFBinary = "";
+        public const string ImageHex = "";
+    }
+   
+}
