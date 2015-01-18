e496112931a937a1e69d8fdbd7d14c7da6592364
jhinder <jan@dfragment.net>
2015-01-18 16:26:11 +0100

    Changed project/assembly metadata

--- netfx-cs/db-ticket/Properties/AssemblyInfo.cs
+++ netfx-cs/db-ticket/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("db-ticket")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Library for checking DB tickets")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("dfragment.net")]
 [assembly: AssemblyProduct("db-ticket")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyCopyright("Copyright © dfragment.net 2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
